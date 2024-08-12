9c2c8f34655e8c6429494f730674372e178b76b4
Evg_S <nikelodiym1210@gmail.com>
2024-08-12 16:39:29 +0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import styled from "styled-components";
 import {Button} from "./components/Button.styled";
 import {Theme} from "./components/styles/Theme.styled";
 import {Card} from "./components/Card";
 import {Typography} from "./components/typography/Typography";
 import {Wrapper} from "./components/Wrapper";
 
+import image from './images/Logo.png'
 
 
 function App() {
     return (
         <div className="App">
-<Card>
-    <img src="/images/Logo.png" alt=""/>
-    <Wrapper maxWidth={'26rem'}>
-        <Typography as="h1">Headline</Typography>
-        <Typography as="p" color={Theme.colors.typographyLight}>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Typography>
-    </Wrapper>
-    <Wrapper>
-        <Button primary>See more</Button>
-        <Button outlined>Save</Button>
-    </Wrapper>
-</Card>
+            <Card>
+                <img src={image} alt=""/>
+                <Wrapper maxWidth={'26rem'}>
+                    <Typography as="h1">Headline</Typography>
+                    <Typography as="p" color={Theme.colors.typographyLight}>Faucibus. Faucibus. Sit sit sapien sit
+                        tempusrisu ut. Sit molestie ornare in venen.</Typography>
+                </Wrapper>
+                <Wrapper>
+                    <Button primary>See more</Button>
+                    <Button outlined>Save</Button>
+                </Wrapper>
+            </Card>
         </div>
     );
 }
